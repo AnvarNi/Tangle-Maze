cdaded75acd72e47090d51470af481017d291cb9
KyreOn <kireonbk@gmail.com>
2021-12-27 18:51:44 +0500

    Level Completion

--- Maze/Assets/ProjectGame/Scripts/Trigger.cs
+++ Maze/Assets/ProjectGame/Scripts/Trigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,7 @@ public class Trigger : MonoBehaviour
     {
         playerLook = player.GetComponent<PlayerLook>();
     }
+    
 
     void OnTriggerEnter(Collider other)
     {
